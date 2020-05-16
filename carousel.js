2a840c710b4a8864c30e96c3e787b7ce142f1ee6
soyoung <soyoungp02@gmail.com>
2020-05-16 23:12:12 +0900

    add tap event and set next card

--- carousel.js
+++ carousel.js
@@ -12,8 +12,17 @@ class Carousel {
 
     // get top card
     this.topCard = this.cards[this.cards.length - 1];
+    // get next card
+    this.nextCard = this.cards[this.cards.length - 2];
     
     if (this.cards.length > 0) {
+      // set default top card position and scale
+      this.topCard.style.transform =
+        'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(1)';
+      
+      // destroy previous Hammer instance, if present
+      if (this.hammer) this.hammer.destroy();
+
       // listen for pan gesture on top card
       this.hammer = new Hammer(this.topCard);
       this.hammer.add(
@@ -22,10 +31,33 @@ class Carousel {
           threshold: 0
         })
       );
-        
-      // pass event data to custom callback
-      this.hammer.on("pan", this.onPan.bind(this));
+
+      // pass event data to custom callbacks
+      this.hammer.on('tap', e => this.onTap(e));
+      this.hammer.on("pan", e => this.onPan(e));
     }
+  }
+
+  onTap() {
+    // get finger position on top card
+    let propX = (e.center.x - e.target.getBoundingClientRect().left) / e.target.clientWidth;
+
+    // get degree of Y rotation (+/-15 degrees)
+    let rotateY = 15 * (propX < 0.05 ? -1 : 1);
+
+    // change the transition property
+    this.topCard.style.transition = 'transform 100ms ease-out';
+
+    // rotate
+    this.topCard.style.transform =
+      `translateX(-50%) translateY(-50%) rotate(0deg) rotateY(${rotateY}deg) scale(1)`;
+
+    // wait transition end
+    setTimeout(() => {
+      // reset transform properties
+      this.topCard.style.transform =
+        'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(1)';
+    }, 100);
   }
     
   onPan(e) {
@@ -52,8 +84,9 @@ class Carousel {
     let posX = e.deltaX + this.startPosX;
     let posY = e.deltaY + this.startPosX;
 
-    // get ratio between swiped pixels and X axis
+    // get ratio between swiped pixels and the axes
     let propX = e.deltaX / this.board.clientWidth;
+    let propY = e.deltaY / this.board.clientHeight;
 
     // get swipe direction, left (-1) or right (1)
     let dirX = e.deltaX < 0 ? -1 : 1;
@@ -61,18 +94,67 @@ class Carousel {
     // get degrees of rotation between 0 and +/- 45
     let deg = this.isDraggingFrom * dirX * Math.abs(propX) * 45;
 
-    // move card
-    this.topCard.style.transform = 'translateX(' + posX + 'px) translateY(' + posY + 'px) rotate(' + deg + 'deg)';
+    // calculate scale ratio, between 95 and 100 %
+    let scale = (95 + 5 * Math.abs(propX)) / 100;
+
+    // move top card
+    this.topCard.style.transform = `translateX(${posX}px) translateY(${posY}px) rotate(${deg}deg) rotateY(0deg) scale(1)`;
+
+    // scale next card
+    if (this.nextCard) {
+      this.nextCard.style.transform = `translateX(-50%) translateY(-50%) rotate(0deg) scale(${scale})`;
+    }
 
     if (e.isFinal) {
       this.isPanning = false;
+      let successful = false;
 
       // set back transition property
       this.topCard.style.transition = 'transform 200ms ease-out';
+      if (this.nextCard)
+        this.nextCard.style.transition = 'transform 100ms linear';
 
-      // reset card position
-      this.topCard.style.transform = 'translateX(-50%) translateY(-50%)';
+      // check threshold
+      if (propX > 0.25 && e.direction == Hammer.DIRECTION_RIGHT) {
+        successful = true;
+        // get right border position
+        posX = this.board.clientWidth;
+      } else if (propX < -0.25 && e.direction == Hammer.DIRECTION_LEFT) {
+        successful = true;
+        posX = - (this.board.clientWidth + this.topCard.clientWidth);
+      } else if (propY < -0.25 && e.direction == Hammer.DIRECTION_UP) {
+        successful = true;
+        // get top border position
+        posY = - (this.board.clientHeight + this.topCard.clientHeight);
+      }
+
+      if (successful) {
+        // throw card in the chosen direction
+        this.topCard.style.transform = `translateX(${posX}px) translateY(${posY}px) rotate(${deg}deg)`;
+
+        // wait transition end
+        setTimeout(() => {
+          this.board.removeChild(this.topCard);
+          this.push();
+          this.handle();
+        }, 200);
+      } else {
+        // reset card position
+        this.topCard.style.transform = 'translateX(-50%) translateY(-50%)';
+        if (this.nextCard)
+          this.nextCard.style.transform =
+            'translateX(-50%) translateY(-50%) rotate(0deg) rotateY(0deg) scale(0.95)';
+      }
     }
+  }
+
+  push() {
+    let card = document.createElement('div')
+    card.classList.add('card');
+
+    card.style.backgroundImage = "url('https://picsum.photos/320/320/?random=" + Math.round(Math.random() * 1000000) + "')";
+
+    this.board.insertBefore(card, this.board.firstChild);
   }
 }
 
